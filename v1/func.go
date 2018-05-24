8af7634f4f1a83b86b39f986f10154b4dd5a5947
Peter Jausovec <peter.jausovec@gmail.com>
2018-05-24 07:02:15 -0700

    inital

---
+++ v1/func.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+
+	fdk "github.com/fnproject/fdk-go"
+)
+
+func main() {
+	fdk.Handle(fdk.HandlerFunc(myHandler))
+}
+
+func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
+	msg := struct {
+		Msg string `json:"message"`
+	}{
+		Msg: "hello v1",
+	}
+	json.NewEncoder(out).Encode(&msg)
+}
